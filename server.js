41f44b4a28380d11626d9f4511de9ddb0c9b1829
Jennifer Duey <jlduey@gmail.com>
2019-07-16 21:16:22 -0400

    copied code from other folder

---
+++ server.js
@@ -0,0 +1,41 @@
+//add dependencies
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var logger = require("morgan");
+
+//initialize express app
+var express = require("express");
+var app = express();
+
+// set up logger for development
+app.use(logger("dev"));
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
+//connect to public
+app.use(express.static(process.cwd() + "/public"));
+
+
+//set app engine
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({defaultLayout: "main"})
+);
+app.set("view-engine", "handlebars");
+
+//set up mongoose database connection
+mongoose.connect("mongodb: //localhost/All-the-News-That-s-Fit-to-Scrape");
+var db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+    console.log("Connected to Mongoose!");
+});
+
+//set up port
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+    console.log("Listening on port " + port);
+});
+
